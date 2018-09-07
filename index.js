10f796b10828256c44a4a50c8623b62eadeff6f8
Cherita Lashley <lashleynook@gmail.com>
2018-09-07 02:58:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function whisper(string) {
   return string.toLowerCase();
 }
 
-function logShout('hello'){
-  console.log(hello.toUpperCase);
+function logShout(string){
+  console.log('hello'.toUpperCase);
 }
 
 function logWhisper(string){
