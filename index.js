41f713af2f1af06d66f9fb5805895e040bb519f6
Cherita Lashley <lashleynook@gmail.com>
2018-09-07 03:07:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  return "hello";
+  return "I can\'t hear you!";
 }
 
 function sayHiToGrandma(string){
-  return "HELLO";
+  return "YES INDEED!";
 }
